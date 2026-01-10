03aef9cf7e3d4bc0df18977f73f244ea2f2c2834
Harshit Tomar <tomarharshit2023@gmail.com>
2026-01-10 14:01:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6853344153-9af1e",
+  "appId": "1:794162969868:web:1aa81a85f0fdff84eb9d29",
+  "apiKey": "AIzaSyBriLfxnG5OKfQiGgn8tBxn99lUySRyl_M",
+  "authDomain": "studio-6853344153-9af1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "794162969868"
+};
